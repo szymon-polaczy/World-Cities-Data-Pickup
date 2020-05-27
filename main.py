c05e14ba2ea61d7972a81a156787bad7c4a9b070
szymon-polaczy <polaczyszymon@gmail.com>
2020-05-27 12:54:00 +0200

    Getting clean data from 4 cities
    Made so much if-statements because I need to clean the data from the page sa that it all makes sense and is easily formated into json - end format that I'm gonna use to save all this data

--- main.py
+++ main.py
@@ -2,20 +2,17 @@ from lxml import html
 import requests
 
 page = requests.get('https://pl.wikipedia.org/wiki/Adrano')
-#page = requests.get('https://en.wikipedia.org/wiki/Nysa,_Poland')
+#page = requests.get('https://pl.wikipedia.org/wiki/Nysa')
+#page = requests.get('https://pl.wikipedia.org/wiki/Aci_Catena')
+#page = requests.get('https://pl.wikipedia.org/wiki/Nowa_S%C3%B3l')
 tree = html.fromstring(page.content)
 
-#ogtable = tree.xpath('//table[@class="infobox geography vcard"]/tbody/tr/td/descendant-or-self::text()')
 ogtable = tree.xpath('//table[@class="infobox"]/tbody/tr/td/descendant-or-self::text()')
 
-#for ele in ogtable:
- #   print(ele)
-
-#print(ogtable)
 
 i = 0
 table = []
-for ele in ogtable:
+for ele in ogtable: #LOOP FOR CLEANING
     ele = ele.replace("\n", '')
     ele = ele.replace("•", '')
     ele = ele.replace("\xa0", ' ')
@@ -23,17 +20,75 @@ for ele in ogtable:
     ele = ele.replace("—", '')
     ele = ele.replace("(", '')
     ele = ele.replace(")", '')
+    ele = ele.replace(":", '')
     ele = ele.replace("\ufeff", '')
     ele = ele.strip()
     if (ele != '' and ele != ' '):
-        table.append(ele)
-    #table.append(ele)
+        if (ele.find("′E") != -1 or ele.find("′N") != -1 or ele.find('°N') != -1 or ele.find('°E') != -1 or ele.find('″N') != -1 or ele.find('″E') != -1):
+            print("Deleted unused coordinates")
+        elif (ogtable[i-1].find("Położenie") != -1 or ogtable[i-3].find("Położenie") != -1):
+            print("Deleted unused data")
+        elif (ele.find("Położenie") != -1):
+            print("Deleted unused data")
+            table.append("Położenie")
+        elif (ogtable[i].find("Kod") != -1 and ogtable[i+1].find("ISTAT") != -1):
+            print("Connecting two places - KOD ISTAT")
+            ogtable[i+1] = "Kod " + ogtable[i+1]
+        elif (ogtable[i].find("TERC") != -1 and ogtable[i+2].find("TERYT") != -1):
+            print("Connecting two places - TERC (TERYT)")
+            ogtable[i+2] = "TERC | " + ogtable[i+2]
+        elif (ogtable[i].find("Urząd miejski") != -1):
+            print("Connecting two places - URZĄD MIEJSKI")
+            ogtable[i+2] = ogtable[i+1] + ' | ' + ogtable[i+2]
+            table.append(ele)
+            del ogtable[i+1]
+        elif (ele.find("[") != -1 and ele.find("]") != -1):
+            print("Deleted unused data")
+        elif (ele.find("Flaga") != -1 or ele.find("Herb") != -1):
+            print("Deleted unused data")
+        elif (ele.find("Multimedia") != -1):
+            break
+        else:
+            table.append(ele)
     i+=1
 
-print(table)
-#
-#for ele in table:
- #   ele = ele.replace('\n', ' ')
-   # print(ele)
+#ANOTHER LOOP FOR END CLEANING
+i = 0
+for ele in table:
+    if (ele == "Położenie"):
+        print("Connecting two places - COORDINATES")
+        table[i+1] = table[i+1] + "°N | " + table[i+2] + "°E"
+        del table[i+2]
+    elif (table[i].find("miasto") != -1 and table[i+1] == 'i' or table[i].find("miasto") != -1 and table[i+1] == 'w' ):
+        print("Connecting two places - TYPE OF PLACE")
+        table[i] = table[i] + ' ' + table[i+1] + ' ' + table[i+2]
+        del table[i+1]
+        del table[i+1]
+    elif (table[i] == ','):
+        del table[i]
+    elif (table[i-1].find("miasto") != -1):
+        while (table[i].find("Państwo") == -1):
+            del table[i]
+    i+=1
 
-#print('Table', table)
+
+#loop for new table
+ntable = []
+j = 0
+k = -1
+for ele in table:
+    if (j % 2 == 0):
+        ntable.append([])
+        k+=1
+
+    ntable[k].append(ele)
+    j+=1
+        
+
+#print(table)
+#print("\n\n\n")
+
+for ele in ntable:
+    print(ele)
+
+#print(ntable)
